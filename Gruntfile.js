aff748feeec6817108f01d60e404c27c88a58f0f
Rachana DS <r.dhalli.srinivasa@accenture.com>
2019-02-11 15:00:50 +0530

    pushed new files 2.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,28 +3,6 @@
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-      htmlmin: {  
-        target: {
-            files: [{
-              expand: true,
-              cwd: 'html',
-              src: ['*.html'],
-              dest: 'htmlminified',
-              ext: '.min.html'
-            }]
-          }                                
-        // dist: {                                      
-        //   options: {                                 
-        //     removeComments: true,
-        //     collapseWhitespace: true
-        //   },
-        //   files: {                               
-        //     'dest/a.html': 'src/a.html',    
-        //     'dest/b.html': 'src/b.html',
-        //   }
-        // },
-      },
-
       sass: {
         target: {
             files: [{
@@ -35,18 +13,12 @@
               ext: '.css'
             }]
           }     
-        // dist: {
-        //     files: {
-        //         'dest/a.css' : 'src/a.scss'
-        //     }
-        // }
     },
 
     });
   
-    grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-sass');
   
-    grunt.registerTask('default', ['htmlmin','sass']);
+    grunt.registerTask('default', ['sass']);
   
   };
